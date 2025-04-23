76e0bc0741621785a704e54dd8a641f238dcdcaf
zahidulsilam <zahidislam08@gmail.com>
2025-04-24 00:37:06 +0200

    task Done

---
+++ zahid-team-leader-question1.py
@@ -0,0 +1,21 @@
+from base_class import base_class
+base_function = base_class()
+
+try:
+    menu_list = base_function.menu_list()
+    for menu in menu_list:
+        print(f"{menu["text"]}\n")
+except Exception as e:
+    print(e)
+try:
+    menu_choice = int(input("Please Select from above menu "))
+    if(menu_choice == 1):
+        print(base_function.add_new_task())
+    elif(menu_choice == 2):
+        base_function.complete_task()
+    elif(menu_choice == 3):
+        base_function.delete_task()
+    elif(menu_choice == 4):
+        print(base_function.view_list_task())
+except Exception as e:
+        print("You selected Wrong option")
